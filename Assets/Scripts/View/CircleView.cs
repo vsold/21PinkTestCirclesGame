d931091ab6414300391849d6865e4a8a526d5d13
Volodymyr Soldatenko <vovan.ukr@gmail.com>
2016-06-28 18:11:19 +0300

    object pool utility script added, circle view added

---
+++ Assets/Scripts/View/CircleView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleView : MonoBehaviour
+{
+    public float Radius { set; get; }
+    public Color Color {set; get;}
+    public float Speed {set; get;}
+    public int Score {set; get;}
+
+    [SerializeField]
+    private SpriteRenderer renderer;
+
+    private void SetRandomColorValue()
+    {
+        Color = new Color(Random.value, Random.value, Random.value);
+        if (renderer != null)
+        {
+            renderer.color = Color;
+        }
+    }
+
+    void Start ()
+    {
+        SetRandomColorValue();
+    }
+}
