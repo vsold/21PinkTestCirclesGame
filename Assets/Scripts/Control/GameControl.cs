94942b324dc30059a2c62b4a0b780017e68bba8b
Volodymyr Soldatenko <vovan.ukr@gmail.com>
2016-06-28 16:59:40 +0300

    game control added

---
+++ Assets/Scripts/Control/GameControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CirclesGame
+{
+    public class GameControl : MonoBehaviour
+    {
+        readonly GameModel model = new GameModel();
+
+        private void StartGame()
+        {
+            model.Score = 0;
+
+        }
+    }
+}
+
