2daa21de9b083fd7fc44a367a4ff501b8b81f9ed
Volodymyr Soldatenko <vovan.ukr@gmail.com>
2016-06-28 21:49:44 +0300

    user input handler added

---
+++ Assets/Scripts/Control/UserInputHandler.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace CirclesGame
+{
+    public class UserInputHandler : MonoBehaviour
+    {
+        public bool Block { get; set; }
+        private Camera cam;
+        private GameControl controller;
+
+        private const string clickTag = "Circle";
+
+        public void Init(GameControl _controller)
+        {
+            controller = _controller;
+        }
+
+        private void Awake()
+        {
+            cam = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Block)
+            {
+                return;
+            }
+
+            CheckForMouseInput();
+        }
+
+        private void CheckForMouseInput()
+        {
+            if (!Input.GetMouseButtonUp(0))
+            {
+                return;
+            }
+
+            var point = cam.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D collider = Physics2D.OverlapPoint(point);
+
+            if (collider == null)
+            {
+                return;
+            }
+
+            string tag = collider.gameObject.tag;
+
+            if (string.IsNullOrEmpty(tag) || tag != "clickTag")
+                return;
+            NotificationCenter.Instance.PostNotification(this, NotificationName.ON_CIRCLE_CLICK);
+            //controller.OnUserInput(point, collider);
+        }
+    }
+}
