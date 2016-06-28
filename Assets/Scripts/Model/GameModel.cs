c83ee603eb984e075f226c868b4ef64b7c77fab6
Volodymyr Soldatenko <vovan.ukr@gmail.com>
2016-06-28 16:58:22 +0300

    GameModel added

---
+++ Assets/Scripts/Model/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CirclesGame
+{
+    public class GameModel
+    {
+        public readonly Vector2 radiusBounds = new Vector2(10f, 60f);
+        public const float baseSpeed = 100f;
+        public const int startCount = 5;
+        public const float startSpawnTime = 1f;
+
+        public int Score { set; get; }
+    }
+}
